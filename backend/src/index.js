023c598cdc995dc6f6103ca3a52f25a05a9c39c2
abhishek singh <me@example.com>
2024-12-18 14:41:01 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
 
+import path from 'path';
+
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import { app,server } from './lib/socket.js';
@@ -11,6 +13,7 @@ import { app,server } from './lib/socket.js';
 dotenv.config();
 
 const PORT=process.env.PORT;
+const __dirname = path.resolve();
 
 //allows us to extract json data from the request body
 app.use(express.json());  
@@ -21,7 +24,15 @@ app.use(cors({
 }));
 
 app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/messages",messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
  
 
 server.listen(PORT, () => {
