0d5946f5c3416e74c8b1fc239b4dedb6b3c1c80a
Abhishek Singh <singh.421.aspabhiya@gmail.com>
2025-10-29 11:50:17 +0530

    Fix/sending-files-isssues

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,8 +17,8 @@ const PORT=process.env.PORT;
 const __dirname = path.resolve();
 
 //allows us to extract json data from the request body
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));  
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));  
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'; //used to make HTTP requests
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === 'development' ? "http://localhost:5001/api" : "/api",
     withCredentials: true,
+    timeout: 120000,
 });
 
-//The code creates a reusable Axios instance with a `baseURL` that switches between a local server (`http://localhost:5001/api`) in development and `/api` in production, determined by `import.meta.env.MODE`. The `withCredentials: true` option ensures credentials like cookies are sent with cross-origin requests.
